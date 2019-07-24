af8321e3e5026c99e24659c99c07148cd53e92fc
rpigu-i <8628078+rpigu-i@users.noreply.github.com>
2019-07-24 08:55:58 -0400

    include dependencies for package

--- setup.py
+++ setup.py
@@ -11,6 +11,10 @@ setup(
     package_dir={'': 'src'},
     include_package_data=True,
     packages=find_packages('src'),
+    install_requires = [
+        'requests>2.21.0',
+        'pyyaml>5.0'
+    ],
     entry_points={
         'console_script': [
             'password-sprayer = password_sprayer.__main__:main'
